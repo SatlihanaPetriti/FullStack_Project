4b02b18ceae891a4eb566c3ea6dfc76728ea8222
satlihana_p <satlihanapetriti@gmail.com>
2026-02-23 21:14:58 +0100

    CRUD -TEST IN ADMIN

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,21 +3,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { Product } from './products/Entity/product.entity';
 import { ProductVariant } from './products/Entity/product-variant.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',           
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Inna1998',
-      database: 'plantshop',   
-      entities: [Product, ProductVariant],
-      synchronize: true,        
-      }),ProductsModule,            
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Product, ProductVariant],
+        synchronize: true,
+      }),
+    }),
+
+    ProductsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule { }
