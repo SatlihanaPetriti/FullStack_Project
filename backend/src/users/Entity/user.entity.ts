f03e71b612581a9c06b64ee283cb078c12d11713
s.p <101753975+SatlihanaPetriti@users.noreply.github.com>
2026-04-07 10:35:43 +0200

    Change resetToken to resetTokenExpires with timestamp

--- backend/src/users/Entity/user.entity.ts
+++ backend/src/users/Entity/user.entity.ts
@@ -20,7 +20,7 @@ export class UserEntity{
     @Column({ type: 'enum', enum: ['user', 'admin'], default: 'user' })
     role: string;
 
-    @Column({ type: 'varchar', nullable: true, default: null })
-    resetToken?: string | null;
+    @Column({ type: 'timestamp', nullable: true })
+    resetTokenExpires?: Date | null;
 
 }
